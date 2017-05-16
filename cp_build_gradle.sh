fbe667447c918587a79e42d578de78a846cb1529
Yue, Liang <liang.yue@cienet.com.cn>
2017-05-16 11:02:17 +0800

    Added Sip and update complie

--- cp_build_gradle.sh
+++ cp_build_gradle.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-root_build_gradle="/home/liang/daily-build-tools/HomePanelBuildGradle/build.gradle"
-app_build_gradle="/home/liang/daily-build-tools/appBuildGradle/build.gradle"
+
 local_properties="/home/liang/daily-build-tools/local.properties"
-#local_arm="/home/liang/HomePanel/SourceCode/HomePanel/app/src/main/obj/local"
-project_path="/home/liang/HomePanel/SourceCode/HomePanel"
-local_arm="$project_path/app/src/main/obj/local"
 
-cp $local_properties $project_path
-cp $root_build_gradle $project_path
-cp $app_build_gradle "$project_path/app"
-rm -rf $local_arm
+homepanel_build_gradle="/home/liang/daily-build-tools/HomePanelBuildGradle/build.gradle"
+homepanel_app_build_gradle="/home/liang/daily-build-tools/HomePanelAppBuildGradle/build.gradle"
+homepanel_project_path="/home/liang/HomePanel/SourceCode/HomePanel"
+homepanel_local_arm="$homepanel_project_path/app/src/main/obj/local"
 
+sip_build_gradle="/home/liang/daily-build-tools/SipBuildGradle/build.gradle"
+sip_app_build_gradle="/home/liang/daily-build-tools/SipAppBuildGradle/build.gradle"
+sip_project_path="/home/liang/HomePanel/SourceCode/SipApp"
+sip_local_arm="$sip_project_path/app/src/main/obj/local"
+
+cp $local_properties $homepanel_project_path
+cp $local_properties $sip_project_path
+
+cp $homepanel_build_gradle $homepanel_project_path
+cp $homepanel_app_build_gradle "$homepanel_project_path/app"
+
+cp $sip_build_gradle $sip_project_path
+cp $sip_app_build_gradle "$sip_project_path/app"
+
+rm -rf $homepanel_local_arm
+rm -rf $sip_local_arm
--- dailybuild.sh
+++ dailybuild.sh
@@ -2,11 +2,20 @@
 export PATH=$PATH:/home/liang/android-ndk-r13b
 
 HOME_PATH="/home/liang"
+
 HOMEPANEL_DIE="HomePanel/SourceCode/HomePanel"
+SIP_DIE="HomePanel/SourceCode/SipApp"
+
 GRADLE="$HOME_PATH/$HOMEPANEL_DIE/gradlew"
-PROJECT_PATH="$HOME_PATH/$HOMEPANEL_DIE"
-OUTPUT_PATH="$PROJECT_PATH/app/build/outputs/apk"
-DEBUG_APK="$OUTPUT_PATH/app-debug.apk"
+
+HOME_PANEL_PROJECT_PATH="$HOME_PATH/$HOMEPANEL_DIE"
+HOME_PANEL_OUTPUT_PATH="$HOME_PANEL_PROJECT_PATH/app/build/outputs/apk"
+HOME_PANEL_DEBUG_APK="$HOME_PANEL_OUTPUT_PATH/app-debug.apk"
+
+SIP_PROJECT_PATH="$HOME_PATH/$SIP_DIE"
+SIP_OUTPUT_PATH="$SIP_PROJECT_PATH/app/build/outputs/apk"
+SIP_DEBUG_APK="$SIP_OUTPUT_PATH/app-debug.apk"
+
 #CURRENT_PATH="$(cd `dirname $0`; pwd)"
 CURRENT_PATH="$HOME_PATH/daily-build-tools"
 FTP_SERVER="/Public/homepanel/dailybuild/app"
@@ -14,15 +23,21 @@ FTP_SERVER="/Public/homepanel/dailybuild/app"
 function make()
 {
      source $CURRENT_PATH/svn_checkout_homepanel.sh
-     chmod -R 777 $PROJECT_PATH
+     chmod -R 777 $HOME_PANEL_PROJECT_PATH
+     chmod -R 777 $SIP_PROJECT_PATH
      source $CURRENT_PATH/cp_build_gradle.sh
 
-     $GRADLE -p $PROJECT_PATH clean
-     $GRADLE -p $PROJECT_PATH build
+     $GRADLE -p $HOME_PANEL_PROJECT_PATH clean
+     $GRADLE -p $HOME_PANEL_PROJECT_PATH build
      notify $?
-     mv $DEBUG_APK "$1/debug-apk-`date +%Y%m%d%H%M`.apk"
+     mv $HOME_PANEL_DEBUG_APK "$1/homepanel-debug-apk-`date +%Y%m%d%H%M`.apk"
+
+     $GRADLE -p $SIP_PROJECT_PATH clean
+     $GRADLE -p $SIP_PROJECT_PATH build
+     notify $?
+     mv $SIP_DEBUG_APK "$1/sip-debug-apk-`date +%Y%m%d%H%M`.apk"
+
      ftp_upload $1
-     $GRADLE -p $PROJECT_PATH clean
 }
 
 function notify()
