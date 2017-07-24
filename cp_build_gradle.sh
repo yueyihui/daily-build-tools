61f07a9922d4edf23b55e0aa6170835ebfd9efaa
Yue, Liang <liang.yue@cienet.com.cn>
2017-07-24 15:33:03 +0800

    change ndk-build to ndk-build.cmd
    1.remove all dir named xAppBuildGradle
    2.del cp xAppBuildGradle in cp_build_gradle.sh

--- cp_build_gradle.sh
+++ cp_build_gradle.sh
@@ -5,13 +5,8 @@ cp "$CURRENT_PATH/local.properties" $SIP_PROJECT_PATH
 cp "$CURRENT_PATH/local.properties" $FINAL_TEST_PROJECT_PATH
 
 cp "$CURRENT_PATH/HomePanelBuildGradle/build.gradle" $HOME_PANEL_PROJECT_PATH
-cp "$CURRENT_PATH/HomePanelAppBuildGradle/build.gradle" "$HOME_PANEL_PROJECT_PATH/app"
-
 cp "$CURRENT_PATH/SipBuildGradle/build.gradle" $SIP_PROJECT_PATH
-cp "$CURRENT_PATH/SipAppBuildGradle/build.gradle" "$SIP_PROJECT_PATH/app"
-
 cp "$CURRENT_PATH/FinalTestBuildGradle/build.gradle" $FINAL_TEST_PROJECT_PATH
-cp "$CURRENT_PATH/FinalTestAppBuildGradle/build.gradle" "$FINAL_TEST_PROJECT_PATH/app"
 
 rm -rf "$HOME_PANEL_PROJECT_PATH/app/src/main/obj/local"
 rm -rf "$SIP_PROJECT_PATH/app/src/main/obj/local"
