b8b66dd7bd6994e566dc9eb9a8dcf3e92a798547
Yue, Liang <liang.yue@cienet.com.cn>
2017-08-10 17:57:01 +0800

    bug fix:
    1.duplicate apk.
    2.method return invaild.

--- dailybuild.sh
+++ dailybuild.sh
@@ -4,27 +4,31 @@ FTP_SERVER_IMG="/Public/homepanel/dailybuild/img"
 ANDROID_SRC="/home/liang/android6.01Src_20170602/android"
 CURRENT_PATH="$( cd "$( dirname "$0" )" && pwd )"
 
-source $CURRENT_PATH/apk_dailybuild.sh
-
-if [ $? -eq 0 ]; then
-	cp $HOME_PANEL_DEBUG_APK $ANDROID_SRC/packages/apps/HomePanel
-	cp $SIP_DEBUG_APK $ANDROID_SRC/packages/apps/SipApp
-	cp $FINAL_TEST_DEBUG_APK $ANDROID_SRC/packages/apps/FinalTest
-	cd $ANDROID_SRC
-	source build/envsetup.sh && lunch 24
-	make -j8 2>&1 | tee build_log
-	if [ $? -eq 0 ]; then
-		pack 2>&1 | tee pack_log
-		if [ $? -eq 0 ]; then
-			ftp_upload "/home/liang/android6.01Src_20170602/lichee/tools/pack/sun50iw1p1_android_p1_uart0.img" $FTP_SERVER_IMG
-		else
-			ftp_upload "/home/liang/android6.01Src_20170602/android/pack_log" $FTP_SERVER_IMG
-		fi
-	else
-		ftp_upload "/home/liang/android6.01Src_20170602/android/build_log" $FTP_SERVER_IMG
-	fi
+function make_android()
+{
+if [ $1 -eq 0 ]; then
+    cp $HOME_PANEL_DEBUG_APK $ANDROID_SRC/packages/apps/HomePanel
+    cp $SIP_DEBUG_APK $ANDROID_SRC/packages/apps/SipApp
+    cp $FINAL_TEST_DEBUG_APK $ANDROID_SRC/packages/apps/FinalTest
+    cd $ANDROID_SRC
+    source build/envsetup.sh && lunch 24
+    make -j8 > build_log 2>&1
+    if [ $1 -eq 0 ]; then
+        pack > pack_log 2>&1
+        if [ $1 -eq 0 ]; then
+            ftp_upload "/home/liang/android6.01Src_20170602/lichee/tools/pack/sun50iw1p1_android_p1_uart0.img" $FTP_SERVER_IMG
+        else
+            ftp_upload "/home/liang/android6.01Src_20170602/android/pack_log" $FTP_SERVER_IMG
+        fi
+    else
+        ftp_upload "/home/liang/android6.01Src_20170602/android/build_log" $FTP_SERVER_IMG
+    fi
 else
-	local log_file="log_`date +%Y%m%d`";
-	echo "apk failed to build some targets " > "$CURRENT_PATH/log_file"
-	echo "plase to view the log from ftp://159.99.249.113/Public/homepanel/dailybuild/app/`date +%Y%m%d`" >> "$CURRENT_PATH/log_file"
+    notify 1
+    echo "retval is $1" > "$TEMP_PATH/android_make_log"
+    echo "apk failed to build some targets " >> "$TEMP_PATH/android_make_log"
 fi
+}
+
+source $CURRENT_PATH/apk_dailybuild.sh
+make_android $ret
