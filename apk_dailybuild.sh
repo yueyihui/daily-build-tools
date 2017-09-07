5a29a28cdccc7801f63d8894e9338125e4779b21
Yue, Liang <liang.yue@cienet.com.cn>
2017-09-07 11:21:24 +0800

    deleted build.gradle
    140 now under jdk-8, we can get gradle up to date.

--- apk_dailybuild.sh
+++ apk_dailybuild.sh
@@ -174,13 +174,13 @@ function svn_go()
 
 function cp_build_gradle()
 {
-    cp "$CURRENT_PATH/local.properties" $HOME_PANEL_PROJECT_PATH
-    cp "$CURRENT_PATH/local.properties" $SIP_PROJECT_PATH
-    cp "$CURRENT_PATH/local.properties" $FINAL_TEST_PROJECT_PATH
+#    cp "$CURRENT_PATH/local.properties" $HOME_PANEL_PROJECT_PATH
+#    cp "$CURRENT_PATH/local.properties" $SIP_PROJECT_PATH
+#    cp "$CURRENT_PATH/local.properties" $FINAL_TEST_PROJECT_PATH
 
-    cp "$CURRENT_PATH/HomePanelBuildGradle/build.gradle" $HOME_PANEL_PROJECT_PATH
-    cp "$CURRENT_PATH/SipBuildGradle/build.gradle" $SIP_PROJECT_PATH
-    cp "$CURRENT_PATH/FinalTestBuildGradle/build.gradle" $FINAL_TEST_PROJECT_PATH
+#    cp "$CURRENT_PATH/HomePanelBuildGradle/build.gradle" $HOME_PANEL_PROJECT_PATH
+#    cp "$CURRENT_PATH/SipBuildGradle/build.gradle" $SIP_PROJECT_PATH
+#    cp "$CURRENT_PATH/FinalTestBuildGradle/build.gradle" $FINAL_TEST_PROJECT_PATH
 
     rm -rf "$HOME_PANEL_PROJECT_PATH/app/src/main/obj/local"
     rm -rf "$SIP_PROJECT_PATH/app/src/main/obj/local"
